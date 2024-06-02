4abb2d6ec55d248e0cae85d599a0b93189691c80
lzglzglzglzg <2394391141@qq.com>
2024-06-02 18:49:28 +0800

    new

--- code/model/utils.py
+++ code/model/utils.py
@@ -28,14 +28,23 @@ def preprocess_train(folder_path):
     return data, labels
 
 def preprocess_test(folder_path):
+    # labels = []
     data = []
     cnt = 0
-    for filename in os.listdir(folder_path):
+    files = os.listdir(folder_path)
+    sorted_files = sorted(files, key=lambda x: int(os.path.splitext(x)[0]))
+    for file in sorted_files:
+        file_path = os.path.join(folder_path, file)
         cnt += 1
-        if filename.endswith(".bin"):
-            match = re.search(r'label_(\d+)_', filename)
-            with open(os.path.join(folder_path, filename), 'rb') as file:
+        if file_path.endswith(".bin"):
+            # match = re.search(r'label_(\d+)_', filename)
+            # if match:
+            #     label = int(match.group(1))
+            # else:
+            #     continue
+            with open(file_path, 'rb') as file:
                 data_row_bin = file.read()
+                # labels.append(label)
                 data_row_float16 = np.frombuffer(data_row_bin, dtype=np.float16)  # 原始数据是float16，直接把二进制bin读成float16的数组
                 data_row_float16 = np.array(data_row_float16)
                 data.append(data_row_float16)
